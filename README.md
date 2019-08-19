1580cd74300a1e4b361aa948f6a642a81f7c5f59
yu4hao4 <1792314541@qq.com>
2019-08-19 16:36:24 +0800

    2019-8-19 solution 1108

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # LeetCodeStudy
  对leetcode中文网上题目的练习源码
 ## 题目对应URL
+<a href="https://github.com/yu4hao4/LeetCodeStudy/blob/master/easy/defanging_an_ip_address_1108.java">defanging_an_ip_address_1108</a> : https://github.com/yu4hao4/LeetCodeStudy/blob/master/easy/defanging_an_ip_address_1108.java
--- easy/defanging_an_ip_address_1108.java
+++ easy/defanging_an_ip_address_1108.java
@@ -8,7 +8,13 @@ package easy;
 public class defanging_an_ip_address_1108 {
     class Solution {
         public String defangIPaddr(String address) {
-            return null;
+            String result = "";
+            result = address.replaceAll("\\.", "[.]");
+            return result;
         }
+    }
+    public static void main(String[] args) {
+        defanging_an_ip_address_1108 d = new defanging_an_ip_address_1108();
+        System.out.println(d.new Solution().defangIPaddr("1.1.1.1"));;
     }
 }
